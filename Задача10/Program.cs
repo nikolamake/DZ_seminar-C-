44560e8990802e5277f79fc9a35030673136cbae
nikolamake <112966899+nikolamake@users.noreply.github.com>
2022-09-22 00:00:40 +0300

    Задачи 10,13,15

---
+++ Задача10/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+
+Console.WriteLine("Введите трёхзначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+if((number >= -999 && number <= -100) || (number >= 100 && number <= 999))
+    if(number >= -999 && number <= -100)
+        {
+        int numberTwo = -((number / 10)) % 10;
+ 
+        Console.WriteLine($"Вторая цифра числа {number} равна {numberTwo}."); 
+        }
+    else
+        {int numberTwo = ((number / 10)) % 10;
+ 
+        Console.WriteLine($"Вторая цифра числа {number} равна {numberTwo}.");
+
+        }
+else 
+
+Console.WriteLine($"Введённое число {number} не является трёхзначным!");
---
+++ Задача13/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+
+
+int number = int.Parse(Console.ReadLine());
+
+int ratio = 1; // для уменьшения разряда
+
+int n1 = number / (ratio); // целая часть при  переходе разрядов
+
+int N1 = number % (ratio); // остаток при  qantity переходов
+if(number > 99 || number < -99)
+
+    if(number > 99)
+
+        {
+            while(n1 < 10 || n1 > 999) 
+    
+            {  
+                n1 = number / (ratio);
+
+                ratio = ratio *10;
+             }
+            N1 = n1 % 10;
+     
+            Console.WriteLine($"Третья цифра числа {number} ровна {N1} ");
+        }
+    
+    else
+        {
+            while(n1 > -10 || n1 < -999) 
+    
+            {  
+                n1 = number / (ratio);
+
+                ratio = ratio *10;
+            }
+            N1 = -n1 % 10;
+     
+            Console.WriteLine($"Третья цифра числа {number} ровна {N1} ");
+        }
+
+
+else
+    Console.WriteLine($"Третья цифра числа {number} отсутствует!");
